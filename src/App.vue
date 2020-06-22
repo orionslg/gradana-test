23f99836ed7b78d963f6f564ce356b6f44b119d0
orionslg <solang.orion@gmail.com>
2020-06-22 14:29:48 +0700

    added navbar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,19 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand>Gradana</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collpase id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item>Task 1</b-nav-item>
+          <b-nav-item>Task 2</b-nav-item>
+          <b-nav-item>Task 3</b-nav-item>
+        </b-navbar-nav>
+      </b-collpase>
+    </b-navbar>
     <router-view />
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
